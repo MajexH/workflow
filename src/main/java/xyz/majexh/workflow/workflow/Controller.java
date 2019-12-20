0b80322ea8314e15943bf2c13b4ad72328ff8ea6
majexh <a502982165@gmail.com>
2019-12-20 20:05:26 +0800

    add Controller

--- src/main/java/xyz/majexh/workflow/workflow/Controller.java
+++ src/main/java/xyz/majexh/workflow/workflow/Controller.java
@@ -3,9 +3,11 @@ package xyz.majexh.workflow.workflow;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import xyz.majexh.workflow.workflow.builder.TopologyBuilder;
 import xyz.majexh.workflow.workflow.entity.def.Topology;
 import xyz.majexh.workflow.workflow.entity.running.Chain;
 import xyz.majexh.workflow.workflow.message.MessageController;
+import xyz.majexh.workflow.workflow.receiver.Receiver;
 
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
@@ -24,6 +26,13 @@ public class Controller {
     private ConcurrentHashMap<String, Topology> topologies;
     private ConcurrentHashMap<String, Chain> chainMap;
     private MessageController zmqMessageControllerImpl;
+    private Receiver receiver;
+    private TopologyBuilder builder;
+
+    @Autowired
+    public void setReceiver(Receiver receiver) {
+        this.receiver = receiver;
+    }
 
     @Autowired
     public void setTopologies(ConcurrentHashMap<String, Topology> topologies) {
@@ -38,6 +47,29 @@ public class Controller {
     @Autowired
     public void setZmqMessageControllerImpl(MessageController zmqMessageControllerImpl) {
         this.zmqMessageControllerImpl = zmqMessageControllerImpl;
+    }
+
+    @Autowired
+    public void setBuilder(TopologyBuilder builder) {
+        this.builder = builder;
+    }
+
+    private void loadTopology() {
+        this.topologies.putAll(this.builder.loadTopologies());
+    }
+
+    public void createChain(String name) {
+        Chain chain = new Chain(this.topologies.get(name));
+        this.chainMap.put(chain.getId(), chain);
+        log.info(String.format("%s chain has been created", chain.getId()));
+    }
+
+    private void recv() {
+        ex.submit(() -> {
+            while (true) {
+                receiver.receiveMessage();
+            }
+        });
     }
 
     public void submitTask() {
